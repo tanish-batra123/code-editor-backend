9281f88c8f6c8411673794e239dbe33b353e2764
tanish-batra123 <tanishbatra893@gmail.com>
2026-02-05 20:53:21 -0800

    fix websocket stability for render

--- index.js
+++ index.js
@@ -109,6 +109,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
